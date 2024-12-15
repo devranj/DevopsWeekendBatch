42fdca8ae116902ec5264c83dc9d6472a130c31a
devranj <devopswithranjeet@gmail.com>
2024-12-15 05:05:09 +0000

    Baisc Shell Program Is Added In The Logic

---
+++ basic_program.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This Is A Basic Shell Program 
+
+echo "Welcome To The Devops Classes For Shell Scripting"
+
+#Ask for the user input
+echo "Please enter your name"
+read user_name
+
+#Greet the user
+echo "Hello, $user_name! Welcome to my home"
+
+
+#Just Say A good Bye And Meet Later 
+echo "Its nice to meet you , see you soon"
+
+#End Of Script
+echo "Script Execution Finished"
+
+
+
+
